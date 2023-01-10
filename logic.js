b8bd367b1e161f403681702ad1b9eb103f2e4873
Acsilaa <galeengames@gmail.com>
2023-01-10 20:57:06 +0100

    finished

---
+++ logic.js
@@ -0,0 +1,79 @@
+let dotHang = document.getElementsByClassName("hang");
+let lives = dotHang.length;
+let guessedLetters = 0;
+for(let i = 0; i < dotHang.length; i++){
+    dotHang[i].style.opacity = 0;
+}
+let buttonsDiv = document.getElementById("rightw");
+let statusDiv = document.getElementById("statusw");
+let actualWord;
+actualWord = possibleWords[Math.floor(Math.random() * possibleWords.length)];
+let actualWordArr = actualWord.split("");
+for(let i = 0; i < actualWordArr.length; i++){
+    let sLetter = `<span class="s${actualWordArr[i]}">_</span>`;
+    statusDiv.innerHTML += sLetter;
+}
+let dictionary = "abcdefghijklmnopqrstuvwxyz".toUpperCase().split("");
+function TryLetter(letter){
+    let didGuessRight = false;
+    for(let i = 0; i < actualWordArr.length; i++){
+        if(actualWordArr[i] != letter.toLowerCase()) continue;
+        didGuessRight = true;
+        let classN = "s" + letter.toLowerCase();
+        let fields = document.getElementsByClassName(classN);
+        for(let i = 0; i < fields.length; i++){
+            fields[i].innerHTML = letter;
+            guessedLetters++;
+        }
+        if(guessedLetters == actualWordArr.length) return Victory();
+    }
+    if(didGuessRight) return;
+    // TODO draw hangman part
+    lives--;
+    let partsToDraw = dotHang.length - lives;
+    for(let i = 0; i < partsToDraw; i++){
+        dotHang[i].style.opacity = 1;
+    }
+    if(lives == 0) return Hanged();
+}
+function RegisterLetter(button){
+    button.disabled = true;
+    TryLetter(button.innerHTML);
+}
+for( let i = 0; i < dictionary.length; i++ ){
+    let crrnt = dictionary[i];
+    let letterButton = `<button id="${crrnt}" onclick="RegisterLetter(this)" class="letterbtn">${crrnt}</button>`;
+    buttonsDiv.innerHTML += letterButton;
+}
+function Hanged(){
+    let leftw = document.getElementById("leftw");
+    leftw.style.background = "red";
+    document.getElementById("head").style.background = "red";
+    let btns = document.getElementsByClassName("letterbtn");
+    let spans = document.getElementsByTagName("span");
+    for(let i = 0; i < spans.length; i++){
+        spans[i].innerHTML = actualWordArr[i].toUpperCase();
+    }
+    for(let i = 0; i < btns.length; i++){
+        btns[i].innerHTML = "restart";
+        btns[i].disabled = true;
+        btns[i].onclick = RestartGame;
+        setTimeout(function(){
+            btns[i].disabled = false;
+        }, 900);
+    }
+}
+function RestartGame(){
+    location.reload();
+}
+function Victory(){
+    let btns = document.getElementsByClassName("letterbtn");
+    for(let i = 0; i < btns.length; i++){
+        btns[i].innerHTML = "WON";
+        btns[i].disabled = true;
+        btns[i].onclick = RestartGame;
+        setTimeout(function(){
+            btns[i].disabled = false;
+        }, 900);
+    }
+}
